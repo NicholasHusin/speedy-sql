db53d44497dde3968edb846aa08c370bf1c92142
Nicholas2750 <nicholashusin@gmail.com>
2020-11-11 22:35:59 -0600

    Add icon

--- scripts/sql.js
+++ scripts/sql.js
@@ -1,6 +1,12 @@
-// The `initSqlJs` function is globally provided by all of the main dist files if loaded in the browser.
-// We must specify this locateFile function if we are loading a wasm file from anywhere other than the current html page's folder.
+const correctMessage = `<svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-check" fill="green" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+                        </svg>
+                        Your answer is correct!`;
 
+const wrongMessage = `<svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-x" fill="red" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                      </svg>
+                      Your output is different from what is expected.`;
 
 async function runSql(query) {
   config = {
@@ -62,9 +68,9 @@ window.addEventListener('load', async function() {
     document.getElementById('actual-output').innerHTML = actualOutput;
 
     if (actualOutput === expectedOutput) {
-      document.getElementById('message').innerHTML = "Your solution is correct!";
+      document.getElementById('message').innerHTML = correctMessage;
     } else {
-      document.getElementById('message').innerHTML = "Your solution is wrong!";
+      document.getElementById('message').innerHTML = wrongMessage;
     }
   });
 });
